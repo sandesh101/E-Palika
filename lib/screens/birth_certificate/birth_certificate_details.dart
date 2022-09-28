75050554173418b0c8a425848342ffcad16b561f
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-09-28 11:07:10 +0545

    Made Birth Registration Details Page UI

--- lib/screens/birth_certificate/birth_certificate_details.dart
+++ lib/screens/birth_certificate/birth_certificate_details.dart
@@ -27,89 +27,32 @@ class BirthCertificateDetails extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 8.0),
                 child: Text(
-                  'About Citizenship',
+                  'About Birth Certificate',
                   style: GoogleFonts.poppins(fontSize: 30, color: Colors.white),
                 ),
               ),
               const Divider(
                 indent: 8,
-                endIndent: 230,
+                endIndent: 300,
                 color: Color(0xFFFFD460),
                 thickness: 3.0,
               ),
               Text(
-                '1. One should be atleast 16 years old.',
+                '1. Upto 35 days, the registration is free.',
                 style: GoogleFonts.poppins(fontSize: 20, color: Colors.white),
               ),
 
-              // =======================Types========================
+              // ===============================================
               Text(
-                '2. Types:',
+                '2. After 35 days, one have to pay Rs. 8 for registration.',
                 style: GoogleFonts.poppins(fontSize: 20, color: Colors.white),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '2.1. Citizenship by descent',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '2.2. Naturalized Citizenship',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '2.3. Non-resident Nepalese Citizenship',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '2.4. Honorary Citizenship',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
               ),
 
-              // =======================Required Documents Starts========================
+              // ===============================================
               Text(
-                '3. Required Documents:',
+                '3. You have to bring proof of your citizenship.',
                 style: GoogleFonts.poppins(fontSize: 20, color: Colors.white),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '3.1. Birth Certificate',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '3.2. Citizenship of Mother or Father or Both',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '3.3. Academic Certificate',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20.0),
-                child: Text(
-                  '3.4. Recommendation from Ward Office',
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ),
-              // =======================Required Document Ends========================
             ],
           ),
           const SizedBox(
@@ -122,7 +65,7 @@ class BirthCertificateDetails extends StatelessWidget {
                 buttonColor: const Color(0xFFFFD460),
                 buttonText: 'Continue',
                 onClick: () => {
-                  Navigator.pushNamed(context, 'citizenship_form'),
+                  Navigator.pushNamed(context, 'birthregistration_form'),
                 },
               ),
               Buttons(
