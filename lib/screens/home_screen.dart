291f7df9dca8b9ba2bee0f444a9bbc3ee4f8f03d
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-10-20 23:54:53 +0545

    Display user email on homepage

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,30 +3,41 @@ import 'package:e_palika/screens/login_screen.dart';
 import 'package:e_palika/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF2D4059),
       body: Center(
         child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Image.asset(
               'assets/images/eplogo.png',
-              height: 200,
+              height: 150,
               width: 200,
+            ),
+            Text(
+              'Welcome ${FirebaseAuth.instance.currentUser?.email}',
+              style: GoogleFonts.poppins(
+                  fontSize: 20, color: AppColors.accentColor),
             ),
             Text(
               'Our Services',
               style: GoogleFonts.poppins(fontSize: 50, color: Colors.white),
             ),
             const SizedBox(
-              height: 70,
+              height: 30,
             ),
             Buttons(
               buttonColor: AppColors.secondaryColor,
